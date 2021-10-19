f1c93730f5b6c5bfe36ac37a44c5a117843cffb5
immortal8eing <n.nico4703@gmail.com>
2021-10-19 09:38:09 +0700

    tugas 2 : base admin template

---
+++ resources/views/table.blade.php
@@ -0,0 +1,15 @@
+@extends('adminapp.master')
+
+@section('title','Nico Table')
+    
+@section('content')
+
+<div class="content-wrapper">
+    
+<div class="container-fluid">
+
+<h1>Table</h1>
+</div>
+
+</div>    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/table', function () {
+    return view('table');
 });
